d7235bbc9f1366be4d8166a7bde805c827379918
Will Asciutto <wasciutto@unc.edu>
2022-04-13 14:10:25 -0400

    Update setup to include click entry point.

--- setup.py
+++ setup.py
@@ -10,15 +10,16 @@ setup(name='pacs-sdk',
       include_package_data=True,
       packages=find_packages(),
       install_requires=['certifi>=2021.10.8',
-                        'charset-normalizer>=2.0.10'
-                        'click>=8.0.3'
-                        'idna>=3.3'
-                        'pydicom>=2.2.2'
-                        'PyYAML>=6.0'
-                        'requests>=2.27.1'
+                        'charset-normalizer>=2.0.10',
+                        'click>=8.0.3',
+                        'idna>=3.3',
+                        'pydicom>=2.2.2',
+                        'PyYAML>=6.0',
+                        'requests>=2.27.1',
                         'urllib3>=1.26.7'],
       entry_points='''
       [console_scripts]
+      pacs_downloader=pacs_sdk.image_downloader:get_studies_cli
       ''',
       zip_safe=False
       )
